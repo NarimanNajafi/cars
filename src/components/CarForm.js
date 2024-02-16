f7dc4177ca50637daddf9bf678caa897f4c6a182
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-02-16 16:44:11 +0330

    input, selectore added

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useDispatch, Selector, useSelector } from 'react-redux';
+import { changeName } from '../store';
 
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+  const handleNamechange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+  return (
+    <div className="car-form panel">
+      <h4 className=" subtitle is-3">Add car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="lable">Name</label>
+            <input
+              className="input-is expanded"
+              value={name}
+              onChange={handleNamechange}
+            ></input>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
